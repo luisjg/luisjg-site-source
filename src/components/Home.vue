855b0cad0c3e2c8b7e78f17ac3a9cf13bb47c550
luisjg <contact@luisjg.io>
2020-08-16 17:22:48 -0700

    Adjust padding layout, revise certain text sections, and include call to action in anchor tag.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,34 +2,39 @@
   <div id="home">
     <section id="home-section" class="section">
       <div class="container has-text-centered">
-        <h1 class="title">Hi, I'm Luis!</h1>
+        <h1 class="title pt-1-half">
+          Hello there!
+        </h1>
+        <p class="subtitle">
+          Nice to meet you.
+        </p>
       </div>
     </section>
 
-    <div class="columns is-centered is-mobile pb-4">
+    <div class="columns is-centered is-mobile pb-4 pt-1-half">
       <div class="column is-half-tablet is-four-fifths-mobile">
-        <p class="is-size-5 tab readability-fix">
-          I'm a <strong>Software Engineer</strong> from the
+        <p class="is-size-5 readability-fix">
+          I'm Luis a <strong>Software Engineer</strong> from the
           <strong>Los Angeles</strong> area with <strong>{{ this.diff }} years</strong>
           of experience working as a <strong>Full Stack Web Developer</strong>
           who understands how to translate a <strong>client’s needs</strong>
-          into a straightforward <strong>software design</strong> that follows
+          into a straightforward <strong>software design</strong> that applies
           standard <strong>code conventions</strong>,
           <strong>unit testing</strong>, and
           <strong>extensive documentation</strong>. I have a proven track
           record of <strong>designing</strong> and <strong>developing</strong>
-          modern and <strong>responsive web applications</strong> and
+          modern and <strong>responsive web applications</strong> as well as
           <strong>REST APIs</strong>. Additionally, I'm also an experienced
           <strong>team leader</strong> and <strong>mentor</strong>
-          that understands software is built by a <strong>team</strong> of
+          that understands <strong>software</strong> is built by a <strong>team</strong> of
           unique <strong>individuals</strong> striving to solve one
           <strong>goal</strong>.
         </p>
         <p class="is-size-5 has-text-centered pt-1-half readability-fix"> If you would like to
           know more about me or are interested in collaborating,
-          feel free and send me an <strong><a href="mailto:contact@luisjg.io"
+          feel free and <strong><a href="mailto:contact@luisjg.io"
             rel="noopener noreferrer" target="_blank"
-            aria-label="Send me an e-mail, let's talk."><u>email</u></a></strong>.
+            aria-label="Send me an e-mail, let's talk."><u>send me an email</u></a></strong>.
         </p>
       </div>
     </div>
@@ -37,9 +42,6 @@
 </template>
 
 <style scoped>
-  .tab {
-   text-indent:5%;
-  }
   @media screen and (min-width: 1024px) {
     .readability-fix {
       margin-right: 10%;
